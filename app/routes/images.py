64cb4dd50ae67139b0a89cc7c29b918e4a458de3
Umdoong <umdoongs@gmail.com>
2025-06-02 15:54:04 +0900

    :recycle: refactoring

--- app/routes/images.py
+++ app/routes/images.py
@@ -5,7 +5,7 @@ from config import db
 
 images_blp = Blueprint("images", __name__)
 
-def create_image():
+def create_image_func():
     data = request.get_json()
     image = Image(
         url=data["url"],
@@ -16,26 +16,18 @@ def create_image():
 
     return image
 
-def get_all_images():
-    images = Image.query.all()
-    return [image.to_dict() for image in images]
-
-def get_main_image():
-    image = Image.query.filter_by(type="main").first()
-    return image
-
 @images_blp.route("/image", methods=["POST"])
-def create_image_route():
+def create_image():
     if request.method == "POST":
         try:
-            image = create_image()
+            image = create_image_func()
             return jsonify({"message": f"ID: {image.id} Image Success Create"}), 201
 
         except ValueError:
             return jsonify({"message": "error"}), 400
 
 @images_blp.route("/image/main", methods=["GET"])
-def get_main_image():
+def get_main_image_route():
     if request.method == "GET":
-        image = get_main_image()
+        image = Image.query.filter_by(type="main").first()
         return jsonify({"image": image.url if image.url else None}), 200
