b17d476160720c4f52a753ac037fe4db0eeba6cb
Umdoong <umdoongs@gmail.com>
2025-06-04 13:59:31 +0900

    :bug: fix bug

--- app/routes/choices.py
+++ app/routes/choices.py
@@ -5,19 +5,6 @@ from config import db
 
 choices_blp = Blueprint("choices", __name__)
 
-def create_choice():
-    data = request.get_json()
-    choice = Choices(
-        content=data["content"],
-        sqe=data["sqe"],
-        is_active=data["is_active"],
-        question_id=data["question_id"],
-    )
-    db.session.add(choice)
-    db.session.commit()
-
-    return choice
-
 def get_choices_by_question_id(question_id):
     choices = (
         Choices.query.filter_by(question_id=question_id, is_active=True).order_by(Choices.sqe).all()
@@ -33,7 +20,16 @@ def get_choice_list(question_id):
 def create_choice():
     if request.method == "POST":
         try:
-            choice = create_choice()
+            data = request.get_json()
+            choice = Choices(
+                content=data["content"],
+                sqe=data["sqe"],
+                is_active=data["is_active"],
+                question_id=data["question_id"],
+            )
+            db.session.add(choice)
+            db.session.commit()
+
             return (
                 jsonify(
                     {"message": f"Content: {choice.content} choice Success Create"}
